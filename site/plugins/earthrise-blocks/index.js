84da6040d7e014ba557d8641159a2d14fa2e2f44
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2025-08-11 08:42:23 -0400

    WIP - setting up blocks templates

---
+++ site/plugins/earthrise-blocks/index.js
@@ -0,0 +1,5 @@
+panel.plugin("earthrise/blocks", {
+  blocks: {
+    accordion: `<details ><summary>{{content[0]}}</summary>{{content[1]}}</details>`,
+  },
+});
---
+++ site/plugins/earthrise-blocks/index.php
@@ -0,0 +1,6 @@
+<?php
+Kirby::plugin('earthrise/blocks', [
+  'blueprints' => [
+    'blocks/accordion' => __DIR__ . '/blueprints/blocks/accordion.yml',
+  ],
+]);
