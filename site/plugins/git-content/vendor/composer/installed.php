9b7f1a32dad8b2512c4ffede7117f1c4e000a466
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2025-08-13 00:59:11 -0400

    adds git content plugin

---
+++ site/plugins/git-content/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'thathoff/kirby-git-content',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '85b85d2e94eaf86257d46dd37445e260f8940668',
+        'type' => 'kirby-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'czproject/git-php' => array(
+            'pretty_version' => 'v4.4.0',
+            'version' => '4.4.0.0',
+            'reference' => '3f2c75c6456b03730955a17a457247861390a6aa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../czproject/git-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'getkirby/composer-installer' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../getkirby/composer-installer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'thathoff/kirby-git-content' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '85b85d2e94eaf86257d46dd37445e260f8940668',
+            'type' => 'kirby-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
