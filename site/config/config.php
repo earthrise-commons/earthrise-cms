58dc5f942b114e7edeb338119fbf26d61683db22
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2025-08-13 00:28:33 -0400

    sets up chapter numbering schema

---
+++ site/config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+  'hooks' => [
+    'page.create:after' => function ($page) {
+    
+      // set initial displayName for person pages to match title
+      if (in_array($page->intendedTemplate()->name(), ['person'])) {
+        if ($page->content()->get('displayName')->isEmpty()) {
+          $page->update([
+            'displayName' => $page->title()->value()
+          ]);
+        }
+      }
+    
+
+    
+    },
+
+  ],
+];
