0cee6c9b4fc863859a975f774d8bcf8ad6292f1f
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2025-08-19 22:23:49 -0400

    enables basicAuth

--- site/config/config.php
+++ site/config/config.php
@@ -4,7 +4,9 @@ return [
 // TODO: customize left panel nav options to add individual collections
 // https://getkirby.com/docs/reference/system/options/panel/panel-menu
 
-// custom panel favicon:
+'api'=>['basicAuth' => true],
+
+// custom panel favicon
 // TODO: ask Olivia to create a new favicon
   'panel' => [
     'favicon' => 'assets/ec-favicon.svg',
