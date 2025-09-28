827dc05e973f883e8f2cbdb76ca934d68ab0eafa
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2025-09-27 23:12:30 -0400

    adds kirby plugin to resize images

---
+++ site/plugins/safe-resize/index.php
@@ -0,0 +1,26 @@
+<?php
+
+use Kirby\Cms\App;
+// plugin to safely resize images and return null if the image is missing (for optional image fields)
+App::plugin('earthrise/safe-resize', [
+  'fileMethods' => [
+    'safeResize' => function (int $width = 1200, int $height = null, array $options = []) {
+      if (!$this) {
+        return null; // field empty -> return null
+      }
+
+      $thumb = $this->resize($width, $height, $options);
+
+      if (!$thumb) {
+        return null; // resize failed -> return null
+      }
+
+      return [
+        'url'    => $thumb->url(),
+        'width'  => $thumb->width(),
+        'height' => $thumb->height(),
+        'alt'    => $this->alt()->or(''),
+      ];
+    }
+  ]
+]);
